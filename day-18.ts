2e27f22370b5fca776eb4611b7e072be706d695a
Mahmoud M. Anwar <mamoanwar97@gmail.com>
2023-12-18 12:03:13 +0200

    Update day-18.ts

--- day-18.ts
+++ day-18.ts
@@ -1,3 +1,4 @@
+//The idea from https://github.com/TkDodo/aot-2023/blob/main/18.ts
 type Filter<TItems extends Array<any>, TSearch extends string> =
   TItems extends [infer Head, ...infer Tail]
     ? Head extends TSearch
